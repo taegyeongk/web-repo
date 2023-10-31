3f90ab799079fee06d615061f7f4e56f70297af7
user <user@DESKTOP-3L4B0DT>
2023-10-31 23:13:53 +0900

    10.31 혼공 복습eu..

--- helloWeb/src/main/webapp/js/dom1.js
+++ helloWeb/src/main/webapp/js/dom1.js
@@ -3,39 +3,39 @@
 const fruits = ['수박', '복숭아', '사과', '포도']
 
 //#show>ul>li:수박+li:사과+...
-//createElement, appendChild, innerHTML 속성
+//createElement : 요소 만들기 , appendChild : 하위요소에 추가, innerHTML : html 에 출력
 
 
-//ul생성
+//ul을 생성하겠습니다.
 const ul = document.createElement('ul');
 
-//li생성
-//fruits.forEach(fruit => {
-//	const li = document.createElement('li');
-//	li.innerHTML = fruit;  //<li>수박</li>
-//	ul.appendChild(li);  //<ul><li>수박</ul></li>
-//})
-//document.getElementById('show').appendChild(ul);
+//li를 생성하겠습니다. forEach 문 사용 가능, 혹은 let 으로 일일히 다 선언해줘도 된다. 다만 코드는 최대한 간략하게
+fruits.forEach(fruit => {
+const li = document.createElement('li');
+li.innerHTML = fruit;  //<li>수박</li>
+ul.appendChild(li);  //<ul><li>수박</ul></li>
+})
+document.getElementById('show').appendChild(ul);
 
 
 
-document.getElementById('show').innerHTML = '<ul>';
+//document.getElementById('show').innerHTML = '<ul>';
 
-let li1 = document.createAttribute('li');
-li1.innerHTML = "수박";
-document.querySelector('ul').appendChild(li1);
+//let li1 = document.createAttribute('li');
+//li1.innerHTML = "수박";
+//document.querySelector('ul').appendChild(li1);
 
-let li2 = document.createAttribute('li');
-li2.innerHTML = "복숭아";
-document.querySelector('ul').appendChild(li2);
+//let li2 = document.createAttribute('li');
+//li2.innerHTML = "복숭아";
+//document.querySelector('ul').appendChild(li2);
 
-let li3 = document.createAttribute('li');
-li3.innerHTML = "사과";
-document.querySelector('ul').appendChild(li3);
+//let li3 = document.createAttribute('li');
+//li3.innerHTML = "사과";
+//document.querySelector('ul').appendChild(li3);
 
-let li4 = document.createAttribute('li');
-li4.innerHTML = "포도";
-document.querySelector('ul').appendChild(li4);
+//let li4 = document.createAttribute('li');
+//li4.innerHTML = "포도";
+//document.querySelector('ul').appendChild(li4);
 
 
 
--- helloWeb/src/main/webapp/js/dom2.js
+++ helloWeb/src/main/webapp/js/dom2.js
@@ -10,15 +10,15 @@ const fruits = [
 ]
 
 //table 생성
-const tbl = document.createElement('table');
-const tbd = document.createElement('tbody');
-tbl.setAttribute('border','1');
+const tbl = document.createElement('table');  //table 만들겠습니다.
+const tbd = document.createElement('tbody');  //tbody 만들겠습니다.
+tbl.setAttribute('border','1'); //table 테두리는 1
 
-fruits.forEach(fruit => {  // console.log(fruit);  결과값: {name: "사과", price: 1000},{name: "복숭아", price: 5000},{name: "포도", price: 6000},{name: "망고", price: 9000}
+fruits.forEach(fruit => {  // forEach( 객체=>화살표함수{})console.log(fruit);  결과값: {name: "사과", price: 1000},{name: "복숭아", price: 5000},{name: "포도", price: 6000},{name: "망고", price: 9000}
 	const tr = document.createElement('tr');
 
 	//반복문 안할 경우에는 const td 계속 생성해서 만들어 줘야하는 귀찮음 존재. 코드는 최대한 간단하게 줄이기.
-	for (let prop in fruit) {
+	for (let prop in fruit) {  //for in 반복문 : 객체의 모든 열거 가능한 속성에 대해 반복
 		const tdl = document.createElement('td');
 		tdl.innerHTML = fruit[prop];
 		tr.appendChild(tdl);
